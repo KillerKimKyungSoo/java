d979f3134664c8de0999f5aed5c577080fcc1794
kim01 <linuxmen@naver.com>
2025-06-18 15:05:49 +0900

    2025년 6월 9일

---
+++ src/collection/array/set/HashStart1.java
@@ -0,0 +1,20 @@
+package collection.array.set;
+
+import java.util.Arrays;
+
+public class HashStart1 {
+    public static void main(String[] args) {
+        Integer[] inputArray = new Integer[4];
+        inputArray[0]=1;
+        inputArray[1]=2;
+        inputArray[2]=5;
+        inputArray[3]=8;
+        System.out.println("InputArray = :" + Arrays.toString(inputArray));
+        int searchValue=8;
+        for(int inputValue : inputArray ){
+            if(inputValue == searchValue){
+                System.out.println(inputValue);
+            }
+        }
+    }
+}
